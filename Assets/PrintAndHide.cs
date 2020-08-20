f4f2fa930079fd7f69b6bec29dbe46bec260bbdf
Allegra Lonard <allegra.lonard@student.uts.edu.au>
2020-08-20 15:15:07 +1000

    Revert "100"
    This reverts commit 2b4f7104d770e3e0b917df816915ba7458e71885.

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -6,12 +6,10 @@ public class PrintAndHide : MonoBehaviour
 {
     int i;
     public Renderer rend;
-    int random;
     // Start is called before the first frame update
     void Start()
     {
         i = 0;
-        random = Random.Range(199, 250);
     }
 
     // Update is called once per frame
@@ -19,13 +17,5 @@ public class PrintAndHide : MonoBehaviour
     {
         Debug.Log(gameObject.name + ": " + i);
         i++;
-        if (gameObject.tag == "Blue" && i == random)
-        {
-            rend.enabled = false;
-        }
-        if (gameObject.tag == "Red" && i == 100)
-        {
-            gameObject.SetActive(false);
-        }
     }
 }
